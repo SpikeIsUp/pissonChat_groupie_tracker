4078280d935863131684f39beb09cade89ef55df
Quentin <quentin.guyenot@ynov.com>
2025-11-28 10:02:03 +0100

    creation router statut:non terminé

--- main.go
+++ main.go
@@ -1,2 +1,21 @@
 package main
 
+import(
+	"net/http"
+	"log"
+)
+
+func main() {
+
+	// r devient router
+	r := router.Router
+
+// fichiers statiques
+	fs := http.FileServer(http.Dir("asset"))
+	http.Handle("/asset/", http.StripPrefix("/asset/", fs))
+
+	http.Handle("/", r)
+
+	log.Println("Serveur lancé--> http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
--- router/router.go
+++ router/router.go
@@ -1,1 +1,20 @@
 package router
+
+import (
+	"net/http"
+
+	"github.com/SpikeIsUp/igApi/controller"
+)
+
+func Router() *http.ServeMux {
+	mux := http.Router()
+
+	// routes
+	mux.HandlFunc("/aPropos", controller.APropos)
+	mux.HandlFunc("/categories", controller.Categories)
+	mux.HandlFunc("/collection", controller.Collection)
+	mux.HandlFunc("/favoris", controller.Favoris)
+	mux.HandlFunc("/", controller.Home)
+	mux.HandlFunc("/recherche", controller.Recherche)
+	mux.HandlFunc("/ressources", controller.Ressources)
+}
